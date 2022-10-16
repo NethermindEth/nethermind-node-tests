eeb18ed631686074f4d00beeab7ff497b008f8ce
Kamil Chodoła <kamil@nethermind.io>
2022-10-16 19:48:05 +0200

    temp comment

--- SedgeNodeFuzzer/Fuzzer.cs
+++ SedgeNodeFuzzer/Fuzzer.cs
@@ -2,9 +2,9 @@
 using SedgeNodeFuzzer.Commands;
 using SedgeNodeFuzzer.Helpers;
 
-while(DockerCommands.CheckIfDockerContainerIsCreated("execution") == false)
-{
-    Console.WriteLine(DateTime.Now + ": Waiting for Execution to be started.");
-    Thread.Sleep(60000);
-}
+//while(DockerCommands.CheckIfDockerContainerIsCreated("execution") == false)
+//{
+//    Console.WriteLine(DateTime.Now + ": Waiting for Execution to be started.");
+//    Thread.Sleep(60000);
+//}
 Parser.Default.ParseArguments<FuzzerCommand>(args).WithParsed(t => t.Execute());

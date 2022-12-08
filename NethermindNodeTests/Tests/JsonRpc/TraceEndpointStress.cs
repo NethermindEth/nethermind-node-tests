9c1bcac352931699fd572bfcba49b3ccd5287c2a
Kamil Chodoła <kamil@nethermind.io>
2022-12-08 15:12:56 +0100

    Increase amount

--- NethermindNodeTests/Tests/JsonRpc/TraceEndpointStress.cs
+++ NethermindNodeTests/Tests/JsonRpc/TraceEndpointStress.cs
@@ -13,7 +13,7 @@ namespace NethermindNodeTests.Tests.JsonRpc
     {
         private static readonly NLog.Logger Logger = NLog.LogManager.GetLogger(TestContext.CurrentContext.Test.Name);
 
-        [TestCase(1000)]
+        [TestCase(10000)]
         [Category("JsonRpc")]
         public async Task TraceBlock(int repeatCount)
         {

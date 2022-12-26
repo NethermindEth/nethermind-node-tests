4e2f7fafcd7e5feb995d92ae0da0f77665077828
Kamil Chodoła <kamil@nethermind.io>
2022-12-26 14:14:33 +0100

    Cleanuo

--- NethermindNodeTests/Tests/SyncingNode/SyncTimeMonitor.cs
+++ NethermindNodeTests/Tests/SyncingNode/SyncTimeMonitor.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NethermindNodeTests.Tests.SnapSync
+namespace NethermindNodeTests.Tests.SyncingNode
 {
     [TestFixture]
     public class SyncTimeMonitor : BaseTest

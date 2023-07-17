9b29c47292b5f78673f1751f8d0900b9b368921f
Kamil Chodoła <kamil@nethermind.io>
2023-07-17 16:43:20 +0200

    Add log

--- NethermindNode.Tests/Tests/JsonRpc/Eth/EthCallTests.cs
+++ NethermindNode.Tests/Tests/JsonRpc/Eth/EthCallTests.cs
@@ -98,6 +98,7 @@ public class EthCallTests : BaseTest
     {
         try
         {
+            Console.WriteLine("Sending request with ID: " + id);
             var w3 = new Web3("http://localhost:8545");
 
             var callInput = new CallInput

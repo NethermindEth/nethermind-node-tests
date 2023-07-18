14e0e984de2e5b8985e00d729a32822172ff9b40
Kamil Chodoła <kamil@nethermind.io>
2023-07-18 15:20:59 +0200

    Renmve reading of response on ethcall

--- NethermindNode.Tests/Tests/JsonRpc/Eth/EthCallTests.cs
+++ NethermindNode.Tests/Tests/JsonRpc/Eth/EthCallTests.cs
@@ -95,7 +95,7 @@ public class EthCallTests : BaseTest
                 Data = code
             };
             var result = w3.Eth.Transactions.Call.SendRequestAsync(callInput);
-            var parsed = result.Result.ToString();
+            //var parsed = result.Result.ToString();
 
         }
         catch (AggregateException e)

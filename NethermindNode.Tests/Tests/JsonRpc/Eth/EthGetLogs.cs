f649a5b1616e46f43cd62e8daa7169c6f12d2c17
Kamil Chodoła <kamil@nethermind.io>
2023-07-31 16:48:21 +0200

    Change logs

--- NethermindNode.Tests/Tests/JsonRpc/Eth/EthGetLogs.cs
+++ NethermindNode.Tests/Tests/JsonRpc/Eth/EthGetLogs.cs
@@ -17,7 +17,7 @@ namespace NethermindNode.Tests.Tests.JsonRpc.Eth
     {
         private static readonly NLog.Logger Logger = NLog.LogManager.GetLogger(TestContext.CurrentContext.Test.Name);
 
-        [TestCase(0, 1, 0, 0, 1, Category = "JsonRpcBenchmark,JsonRpcGatewayEthGetLogsBenchmarkStress")]
+        [TestCase(10000, 100, 0, 0, 0, Category = "JsonRpcBenchmark,JsonRpcGatewayEthGetLogsBenchmarkStress")]
         public async Task EthGetLogsGatewayScenario(int repeatCount, int initialRequestsPerSecond, int rpsStep, int stepInterval, int maxTimeout = 0)
         {
             int counter = 0;
@@ -35,8 +35,6 @@ namespace NethermindNode.Tests.Tests.JsonRpc.Eth
                     var response = await responseTask;
                     if (response != null)
                     {
-                        Console.WriteLine("Response is not null... Proceeding.");
-                        Console.WriteLine(response.Length.ToString());
                         foreach (var filterLog in response)
                         {
                             Console.WriteLine($"Removed: {filterLog.Removed}");
@@ -72,14 +70,14 @@ namespace NethermindNode.Tests.Tests.JsonRpc.Eth
                         Console.WriteLine("Response was null");
                     }
 
-                    //if (response.Contains("error") && response.ToString() != String.Empty)
-                    //{
-                    //    fail++;
-                    //}
-                    //else
-                    //{
-                    //    success++;
-                    //}
+                    if (response.Contains("error") && response.ToString() != String.Empty)
+                    {
+                        fail++;
+                    }
+                    else
+                    {
+                        success++;
+                    }
                 }
             });
 

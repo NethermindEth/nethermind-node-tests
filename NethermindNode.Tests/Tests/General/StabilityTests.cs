381ac133ac76b59401deb7071a2582b8673c1734
Kamil Chodoła <kamil.chodola@gmail.com>
2024-09-05 14:25:43 +0200

    Add new blocl production test and imrpvoe stability one

---
+++ NethermindNode.Tests/Tests/General/StabilityTests.cs
@@ -0,0 +1,40 @@
+
+using NethermindNode.Core.Helpers;
+
+namespace NethermindNode.Tests.SyncingNode;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class StabilityTests : BaseTest
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetLogger(TestContext.CurrentContext.Test.Name);
+
+    [Test]
+    [Category("StabilityCheck")]
+    public void ShouldVerifyIfNodePerformsStable()
+    {
+        Logger.Info("***Starting test: ShouldVerifyIfNodePerformsStable***");
+
+        NodeInfo.WaitForNodeToBeReady(Logger);
+
+        bool isNodeSynced = NodeInfo.IsFullySynced(Logger);
+        while (!isNodeSynced)
+        {
+            isNodeSynced = NodeInfo.IsFullySynced(Logger);
+            if (isNodeSynced)
+            {
+                isNodeSynced = true;
+                Thread.Sleep(600000); // Maybe small hack - wait for 10 minutes after node is synced to let it process a few blocks and then check for exceptions as a very last check
+                                      // Also it is nice because some real tests can be started in meantime and be relatively short so there is a chance that stability test will catch some exceptions caused by other tests - this wil still mean that node is not stable
+            }
+            var exceptions = DockerCommands.GetDockerLogs(ConfigurationHelper.Instance["execution-container-name"], "Exception");
+            Assert.IsEmpty(exceptions, "Exception occured: " + exceptions.Select(x => x));
+
+            var corruption = DockerCommands.GetDockerLogs(ConfigurationHelper.Instance["execution-container-name"], "Corrupted");
+            Assert.IsEmpty(corruption, "Corruption occured: " + exceptions.Select(x => x));
+
+        }
+
+        Logger.Info("***Test finished: ShouldVerifyIfNodePerformsStable***");
+    }
+}
---
+++ NethermindNode.Tests/Tests/SyncedNode/BlockProduction.cs
@@ -0,0 +1,29 @@
+using NethermindNode.Core.Helpers;
+
+namespace NethermindNode.Tests.SyncedNode;
+
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class BlockProduction : BaseTest
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetLogger(TestContext.CurrentContext.Test.Name);
+
+    [Test]
+    [Category("BlockProductionSimulationVerification")]
+    public void ShoudlVerifyIfBlockProductionSimualtionWorks()
+    {
+        Logger.Info($"***Starting test: ShoudlVerifyIfBlockProductionSimualtionWorks***");
+
+        NodeInfo.WaitForNodeToBeReady(Logger);
+        NodeInfo.WaitForNodeToBeSynced(Logger);
+
+        Thread.Sleep(120000); //Give it a time to warm up after sync (catching 32 blocks, starting oldBodies etc - it takes short time and in emantime first blocks should appear)
+
+        var blockImprove = DockerCommands.GetDockerLogs(ConfigurationHelper.Instance["execution-container-name"], "Improved post-merge block");
+        Assert.IsNotEmpty(blockImprove, "No block improvements after sync in simulation mode.");
+
+        var blockProduction = DockerCommands.GetDockerLogs(ConfigurationHelper.Instance["execution-container-name"], "Produced block");
+        Assert.IsNotEmpty(blockProduction, "No block production after sync in simulation mode.");
+    }
+}

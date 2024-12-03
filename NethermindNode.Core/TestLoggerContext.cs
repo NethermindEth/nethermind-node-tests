5211cfaa2e3dd7dac9733d5c84e05e56ccc377ff
Kamil Chodoła <kamil.chodola@gmail.com>
2024-12-03 16:31:37 +0100

    Change logger approach

--- NethermindNode.Core/TestLoggerContext.cs
+++ NethermindNode.Core/TestLoggerContext.cs
@@ -1,10 +1,20 @@
 ﻿using NLog;
+using NUnit.Framework;
 
 namespace NethermindNode.Core
 {
     public static class TestLoggerContext
     {
-        [ThreadStatic]
-        public static Logger Logger;
+        private static AsyncLocal<Logger> _logger = new AsyncLocal<Logger>();
+
+        public static Logger Logger
+        {
+            get
+            {
+                var testName = TestContext.CurrentContext?.Test?.Name ?? "UnknownTest";
+                return _logger.Value ?? LogManager.GetLogger(testName);
+            }
+            set => _logger.Value = value;
+        }
     }
 }
